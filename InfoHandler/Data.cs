85b9492e945b449a5e4aa10faa1d52c524282125
Maaici <sj5678910@gmail.com>
2019-12-25 21:38:11 +0800

    修改

--- InfoHandler/Data.cs
+++ InfoHandler/Data.cs
@@ -1,24 +1,61 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
+using System.Data;
+using System.Data.SQLite;
 
 namespace InfoHandler
 {
     public static class Data
     {
-        public static string ConnectionString = "Data Source=c:\\NewsWeb.db";
+        public static string ConnectionString = "Data Source=E:\\MyCode\\Maaici\\InfoHandler\\InfoHandler\\NewsWeb.db";
 
         public static int ExcuteSql(string sql) {
-            SqlConnection mycon = new SqlConnection(ConnectionString);
-            mycon.Open();
-            SqlCommand cmd = new SqlCommand(sql, mycon);
-            int num = cmd.ExecuteNonQuery();
-            mycon.Close();
-            return num;
+            SQLiteConnection mycon = null;
+            try
+            {
+                mycon = new SQLiteConnection(ConnectionString);
+                mycon.Open();
+                SQLiteCommand cmd = new SQLiteCommand(sql, mycon);
+                int num = cmd.ExecuteNonQuery();
+                mycon.Close();
+                return num;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Excute(" + sql + ")Err:" + ex.Message);
+                return -1;
+            }
+            finally
+            {
+                mycon.Close();
+            }
+        }
+
+        public static DataTable SqlTable(string sql)
+        {
+            SQLiteConnection mycon = null ;
+            try
+            {
+                mycon = new SQLiteConnection(ConnectionString); 
+                mycon.Open();
+                SQLiteCommand sqlcmd = new SQLiteCommand(sql, mycon);//sql语句
+                sqlcmd.CommandTimeout = 120;
+                SQLiteDataReader reader = sqlcmd.ExecuteReader();
+                DataTable dt = new DataTable();
+                if (reader != null)
+                {
+                    dt.Load(reader, LoadOption.PreserveChanges, null);
+                }
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("SqlReader(" + sql + ")Err:" + ex.Message );
+                return null;
+            }
+            finally
+            {
+                mycon.Close();
+            }
         }
     }
 }
--- InfoHandler/Program.cs
+++ InfoHandler/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,14 +12,23 @@ namespace InfoHandler
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("开始读取数据。。。。。。");
-            
-            for (int i = 1; i <= 142; i++) {
-                string url = "https://api.eol.cn/gkcx/api/?page="+i+"&request_type=1&size=20&sort=view_total&uri=apigkcx/api/school/hotlists";
+            //GetGaoxiao();
+            GetZhuanye();
+            Console.ReadLine();
+        }
+
+        private static void GetGaoxiao()
+        {
+            Console.WriteLine("开始读取高校数据。。。。。。");
+            //一共 2834 条数据   每页 20 条  要 142 页
+            for (int i = 1; i <= 142; i++)
+            {
+                string url = "https://api.eol.cn/gkcx/api/?page=" + i + "&request_type=1&size=20&sort=view_total&uri=apigkcx/api/school/hotlists";
                 string res = HttpHelper.HttpGet(url);
                 var data = JsonConvert.DeserializeObject<dynamic>(res).data.item;
-                
-                foreach (var item in data) {
+
+                foreach (var item in data)
+                {
                     var school_id = item.school_id.ToString();
                     var name = item.name.ToString();
                     var city_name = item.city_name.ToString();
@@ -30,11 +40,44 @@ namespace InfoHandler
                             values ({school_id},'{name}','{city_name}','{province_name}','{address}','{type_name}','{nature_name}') ";
                     Data.ExcuteSql(sql);
                 }
-                Console.WriteLine("处理第一页 -》 -》 -》");
+                Console.WriteLine($"第 {i} 页处理完成 -》 -》 -》");
             }
-            Console.WriteLine("");
+            Console.WriteLine("++++++++++高校数据读取完成+++++++++++++");
+            Console.WriteLine();
+        }
 
-            Console.ReadLine();
+        private static void GetZhuanye()
+        {
+            Console.WriteLine("开始读取高校专业数据。。。。。。");
+            DataTable data = null;
+            do
+            {
+                //sqlite语法不太一样
+                data = Data.SqlTable(" select school_id from dt_universities where ifnull(state,0) = 0 limit 10 ");
+                //data = Data.SqlTable(" select top 10 school_id from dt_universities where isnull(state,0) = 0 ");
+                foreach (DataRow dr in data.Rows) {
+                    var json = HttpHelper.HttpGet($"https://static-data.eol.cn/www/school/{dr["school_id"].ToString()}/pc_special.json");
+                    var ret = JsonConvert.DeserializeObject<dynamic>(json).special_detail;
+                    Type dtype = ret.GetType();
+                    foreach (var item in dtype.GetProperties()) {
+                        if (item.GetValue(ret).ToString() == "")
+                            continue;
+                        foreach (var ii in item.GetValue(ret))
+                        {
+                            var special_id = ii.special_id.ToString();
+                            var school_id = ii.school_id.ToString();
+                            var special_name = ii.special_name.ToString();
+                            var type_name = ii.type_name.ToString();
+                            string sql = $@"insert into dt_zhuanye ( special_id, school_id ,special_name,type_name) 
+                            values({ special_id},{school_id},'{special_name}','{type_name}' ) ";
+                            Data.ExcuteSql(sql);
+                        }
+                    }
+                }
+            } while (data.Rows.Count > 0);
+            
+            
+            Console.WriteLine("+++++++++Done++++++++++++++");
         }
     }
 }
